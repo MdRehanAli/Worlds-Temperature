e59524e8b5706d644fd97ac2fc9f31b143cdfe18
MdRehanAli <rehanali21d>
2023-11-30 18:35:09 +0600

    Added Dataset and import packages

---
+++ main.py
@@ -0,0 +1,7 @@
+#Worlds Temperature Prediction
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression #Sklearn for the main model & LinearRegression model for regression
